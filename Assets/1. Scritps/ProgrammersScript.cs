484f9e9ee05229291368d8de300004f83b5bf393
young-cod <64890861+young-cod@users.noreply.github.com>
2023-07-05 22:38:26 +0900

    컨트롤 제트

--- Assets/1. Scritps/ProgrammersScript.cs
+++ Assets/1. Scritps/ProgrammersScript.cs
@@ -296,5 +296,37 @@ my_string은 영어 소문자 또는 0부터 9까지의 숫자로 이루어져 있습니다. - - -
             return arrange.Distinct().ToArray();
         }
         #endregion
+
+        #region 컨트롤 제트
+
+        /*
+         * 문제 설명
+숫자와 "Z"가 공백으로 구분되어 담긴 문자열이 주어집니다. 문자열에 있는 숫자를 차례대로 더하려고 합니다. 이 때 "Z"가 나오면 바로 전에 더했던 숫자를 뺀다는 뜻입니다. 숫자와 "Z"로 이루어진 문자열 s가 주어질 때, 머쓱이가 구한 값을 return 하도록 solution 함수를 완성해보세요.
+
+제한사항
+1 ≤ s의 길이 ≤ 200
+-1,000 < s의 원소 중 숫자 < 1,000
+s는 숫자, "Z", 공백으로 이루어져 있습니다.
+s에 있는 숫자와 "Z"는 서로 공백으로 구분됩니다.
+연속된 공백은 주어지지 않습니다.
+0을 제외하고는 0으로 시작하는 숫자는 없습니다.
+s는 "Z"로 시작하지 않습니다.
+s의 시작과 끝에는 공백이 없습니다.
+"Z"가 연속해서 나오는 경우는 없습니다.
+         */
+
+         public static int ControlZ(string s)
+        {
+            int answer = 0;
+            string[] str = s.Split(" ");
+            for (int i = 0; i < str.Length; i++)
+            {
+                answer += str[i] == "Z" ? -int.Parse(str[i - 1]) : int.Parse(str[i]);
+            }
+
+            return answer;
+        }
+
+        #endregion
     }
 }
