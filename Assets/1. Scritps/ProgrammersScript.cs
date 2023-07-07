ae97bdc2907ee552fe3c9e612ab6182cb8fbca48
young-cod <64890861+young-cod@users.noreply.github.com>
2023-07-07 22:20:24 +0900

    가까운 수

--- Assets/1. Scritps/ProgrammersScript.cs
+++ Assets/1. Scritps/ProgrammersScript.cs
@@ -347,7 +347,7 @@ my_string은 대문자, 소문자, 공백으로 구성되어 있습니다.
             string answer = "";
             List<string> list = new List<string>();
 
-            foreach(var item in my_string)
+            foreach (var item in my_string)
             {
                 list.Add(item.ToString());
             }
@@ -362,6 +362,42 @@ my_string은 대문자, 소문자, 공백으로 구성되어 있습니다.
          *  string answer = string.Concat(my_string.Distinct());
             return answer;
          */
+        #endregion
+
+        #region 가까운 수
+        /*
+         * 문제 설명
+정수 배열 array와 정수 n이 매개변수로 주어질 때, array에 들어있는 정수 중 n과 가장 가까운 수를 return 하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ array의 길이 ≤ 100
+1 ≤ array의 원소 ≤ 100
+1 ≤ n ≤ 100
+가장 가까운 수가 여러 개일 경우 더 작은 수를 return 합니다.
+         */
+
+        //[10,11,12,14] , 13
+        public static int NearByNumber(int[] array, int n)
+        {
+            int answer = 0;
+
+            int diff = Mathf.Abs(array[0] - n);
+            answer = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (diff > Mathf.Abs(array[i] - n))
+                {
+                    diff = Mathf.Abs(array[i] - n);
+                    answer = array[i];
+                }
+                else if (diff == Mathf.Abs(array[i] - n) && answer > array[i])
+                {
+                    answer = array[i];
+                }
+            }
+
+            return answer;
+        }
         #endregion
     }
 }
