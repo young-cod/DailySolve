4f46d2d9e4e36714bb19159bfff819bf2925450c
young-cod <64890861+young-cod@users.noreply.github.com>
2023-07-12 22:14:17 +0900

    가장 큰 수 찾기

--- Assets/1. Scritps/ProgrammersScript.cs
+++ Assets/1. Scritps/ProgrammersScript.cs
@@ -651,5 +651,37 @@ s는 소문자로만 이루어져 있습니다.
 
         #endregion
 
+        #region 가장 큰 수 찾기
+
+        /*
+         * 문제 설명
+정수 배열 array가 매개변수로 주어질 때, 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+제한사항
+1 ≤ array의 길이 ≤ 100
+0 ≤ array 원소 ≤ 1,000
+array에 중복된 숫자는 없습니다.
+         */
+        public static int[] solution(int[] array)
+        {
+            int[] answer = new int[2];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == array.Max())
+                {
+                    answer[0] = array[i];
+                    answer[1] = i;
+                }
+            }
+            return answer;
+        }
+
+        //Ohter Solution
+        // int[] answer = new int[2] { array.Max(), Array.IndexOf(array, array.Max()) };
+        //return answer;
+
+        #endregion
+
     }
 }
