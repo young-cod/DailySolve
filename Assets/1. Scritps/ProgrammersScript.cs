1efd235b93b21e27ba15c15029bcb2bfdb42fb4a
young-cod <64890861+young-cod@users.noreply.github.com>
2023-07-17 19:12:17 +0900

    숫자 찾기

--- Assets/1. Scritps/ProgrammersScript.cs
+++ Assets/1. Scritps/ProgrammersScript.cs
@@ -722,5 +722,37 @@ my_string의 숫자와 연산자는 공백 하나로 구분되어 있습니다.
         }
         #endregion
 
+        #region 숫자 찾기
+
+        /*
+         * 문제 설명
+정수 num과 k가 매개변수로 주어질 때, num을 이루는 숫자 중에 k가 있으면 num의 그 숫자가 있는 자리 수를 return하고 없으면 -1을 return 하도록 solution 함수를 완성해보세요.
+
+제한사항
+0 < num < 1,000,000
+0 ≤ k < 10
+num에 k가 여러 개 있으면 가장 처음 나타나는 자리를 return 합니다.
+         */
+
+        public static int FindNumber(int num, int k)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < num.ToString().Length; i++)
+            {
+                if (num.ToString()[i] - '0' == k)
+                {
+                    answer = i+1;
+                    break;
+                }
+            }
+            if (answer == 0) answer = -1;
+            return answer;
+        }
+        //Other Solution
+        //int answer = num.ToString().IndexOf(k.ToString()) + 1;
+
+        #endregion
+
     }
 }
