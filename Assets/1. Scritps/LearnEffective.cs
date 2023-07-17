bd7efbaf69e4d283f880ff66b56e70c6fc951f91
young-cod <64890861+young-cod@users.noreply.github.com>
2023-07-17 20:54:38 +0900

    LearnEffective

---
+++ Assets/1. Scritps/LearnEffective.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnEffective : MonoBehaviour
+{
+    #region 지역 변수를 선언할 때는 var를 사용하는 것이 낫다
+    /*
+     * var : 컴파일 타임에 타입이 결정 됨
+     * dynamic : 런타임에 타입이 결정 됨
+     */
+
+
+    #endregion
+
+}
