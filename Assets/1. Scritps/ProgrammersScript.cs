83f4b3a77a960e479115669a0e1a59b0c43adfda
young-cod <64890861+young-cod@users.noreply.github.com>
2023-07-18 20:23:47 +0900

    세균증식

--- Assets/1. Scritps/ProgrammersScript.cs
+++ Assets/1. Scritps/ProgrammersScript.cs
@@ -832,5 +832,30 @@ X, Y, Z는 0을 제외하고는 0으로 시작하지 않습니다.
 
         #endregion
 
+        #region 세균증식
+
+        /*
+         * 문제 설명
+어떤 세균은 1시간에 두배만큼 증식한다고 합니다. 처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때 t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ n ≤ 10
+1 ≤ t ≤ 15
+         */
+        public static float BacterialGrowth(int n, int t)
+        {
+            float answer = n;
+
+            for (int i = 1; i <= t; i++)
+            {
+                answer *= 2;
+            }
+
+            return answer;
+        }
+        //Other Solution
+        // int answer = n << t;
+        #endregion
+
     }
 }
