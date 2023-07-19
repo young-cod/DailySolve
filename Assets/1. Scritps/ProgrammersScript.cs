3edc210b3dfc1f76d2c532b0d15c7c658fe5522c
young-cod <64890861+young-cod@users.noreply.github.com>
2023-07-19 19:59:26 +0900

    7의 개수

--- Assets/1. Scritps/ProgrammersScript.cs
+++ Assets/1. Scritps/ProgrammersScript.cs
@@ -891,5 +891,38 @@ X, Y, Z는 0을 제외하고는 0으로 시작하지 않습니다.
 
         #endregion
 
+        #region 7의 개수
+
+        /*
+         * 문제 설명
+머쓱이는 행운의 숫자 7을 가장 좋아합니다. 정수 배열 array가 매개변수로 주어질 때, 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
+
+제한사항
+1 ≤ array의 길이 ≤ 100
+0 ≤ array의 원소 ≤ 100,000
+         */
+
+        public static int NumberOfSeven(int[] array)
+        {
+            int answer = 0;
+
+            foreach (var item in array)
+            {
+                for (int i = 0; i < item.ToString().Length; i++)
+                {
+                    if (item.ToString()[i] == '7')
+                    {
+                        answer++;
+                    }
+                }
+            }
+
+            return answer;
+        }
+
+        //Other Solution
+        //int answer = string.Join("", array).Count(x => x == '7');
+
+        #endregion
     }
 }
