f06d3e92a40767c0b8887c70c06928bacf9c0048
young-cod <64890861+young-cod@users.noreply.github.com>
2023-07-24 21:18:06 +0900

    캐릭터의 좌표

--- Assets/1. Scritps/ProgrammersScript.cs
+++ Assets/1. Scritps/ProgrammersScript.cs
@@ -997,12 +997,12 @@ dots의 원소의 길이 = 2
         {
             int answer = 1;
             int[] calc = new int[2];
-            
+
             //x좌표가 같으면 y의 길이
             if (dots[0, 0] == dots[1, 0])
             {
                 calc[1] = Math.Abs(dots[0, 1] - dots[1, 1]);
-                calc[0] = Math.Abs(dots[0, 0]- dots[2, 0]);
+                calc[0] = Math.Abs(dots[0, 0] - dots[2, 0]);
             }
             //y좌표가 같으면 x의 길이
             else
@@ -1034,5 +1034,55 @@ dots의 원소의 길이 = 2
          */
 
         #endregion
+
+        #region 캐릭터의 좌표
+
+        /*
+         * 문제 설명
+머쓱이는 RPG게임을 하고 있습니다. 게임에는 up, down, left, right 방향키가 있으며 각 키를 누르면 위, 아래, 왼쪽, 오른쪽으로 한 칸씩 이동합니다. 예를 들어 [0,0]에서 up을 누른다면 캐릭터의 좌표는 [0, 1], down을 누른다면 [0, -1], left를 누른다면 [-1, 0], right를 누른다면 [1, 0]입니다. 머쓱이가 입력한 방향키의 배열 keyinput와 맵의 크기 board이 매개변수로 주어집니다. 캐릭터는 항상 [0,0]에서 시작할 때 키 입력이 모두 끝난 뒤에 캐릭터의 좌표 [x, y]를 return하도록 solution 함수를 완성해주세요.
+
+[0, 0]은 board의 정 중앙에 위치합니다. 예를 들어 board의 가로 크기가 9라면 캐릭터는 왼쪽으로 최대 [-4, 0]까지 오른쪽으로 최대 [4, 0]까지 이동할 수 있습니다.
+제한사항
+board은 [가로 크기, 세로 크기] 형태로 주어집니다.
+board의 가로 크기와 세로 크기는 홀수입니다.
+board의 크기를 벗어난 방향키 입력은 무시합니다.
+0 ≤ keyinput의 길이 ≤ 50
+1 ≤ board[0] ≤ 99
+1 ≤ board[1] ≤ 99
+keyinput은 항상 up, down, left, right만 주어집니다.
+         */
+
+        public static int[] CoordinateOfCharacter(string[] keyinput, int[] board)
+        {
+            int[] answer = new int[2];
+
+            foreach (var input in keyinput)
+            {
+                switch (input)
+                {
+                    case "up":
+                        answer[1]++;
+                        if (answer[1] > board[1]/2) answer[1] = board[1]/2;
+                        break;
+                    case "down":
+                        answer[1]--;
+                        if (answer[1] < -board[1]/2) answer[1] = -board[1]/2;
+                        break;
+                    case "left":
+                        answer[0]--;
+                        if (answer[0] < -board[0]/2) answer[0] = -board[0]/2;
+                        break;
+                    case "right":
+                        answer[0]++;
+                        if (answer[0] > board[0]/2) answer[0] = board[0]/2;
+                        break;
+                }
+            }
+
+
+            return answer;
+        }
+        #endregion
+
     }
 }
