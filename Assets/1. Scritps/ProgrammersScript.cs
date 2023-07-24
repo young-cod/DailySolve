adb0d3954f3afe7bc1e29bba2b11e8b37c0dae43
young-cod <64890861+young-cod@users.noreply.github.com>
2023-07-24 21:03:49 +0900

    직사각형 넓이 구하기

--- Assets/1. Scritps/ProgrammersScript.cs
+++ Assets/1. Scritps/ProgrammersScript.cs
@@ -979,5 +979,60 @@ my_str은 알파벳 소문자, 대문자, 숫자로 이루어져 있습니다.
          */
 
         #endregion
+
+        #region 직사각형 넓이 구하기
+
+        /*
+         * 문제 설명
+2차원 좌표 평면에 변이 축과 평행한 직사각형이 있습니다. 직사각형 네 꼭짓점의 좌표 [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]가 담겨있는 배열 dots가 매개변수로 주어질 때, 직사각형의 넓이를 return 하도록 solution 함수를 완성해보세요.
+
+제한사항
+dots의 길이 = 4
+dots의 원소의 길이 = 2
+-256 < dots[i]의 원소 < 256
+잘못된 입력은 주어지지 않습니다.
+         */
+
+        public static int FindRectangleArea(int[,] dots)
+        {
+            int answer = 1;
+            int[] calc = new int[2];
+            
+            //x좌표가 같으면 y의 길이
+            if (dots[0, 0] == dots[1, 0])
+            {
+                calc[1] = Math.Abs(dots[0, 1] - dots[1, 1]);
+                calc[0] = Math.Abs(dots[0, 0]- dots[2, 0]);
+            }
+            //y좌표가 같으면 x의 길이
+            else
+            {
+                calc[0] = Math.Abs(dots[0, 0] - dots[1, 0]);
+                calc[1] = Math.Abs(dots[0, 1] - dots[2, 1]);
+            }
+            answer = calc[0] * calc[1];
+
+            return answer;
+        }
+
+        //Other Solution
+        /*
+         *  int answer = 0;
+
+        int[] x = new int[4];
+        int[] y = new int[4];
+
+        for(int i=0; i<4; i++)
+        {
+            x[i] = dots[i, 0];
+            y[i] = dots[i, 1];
+        }
+
+        answer = (x.Max() - x.Min()) * (y.Max() - y.Min());
+
+        return answer;
+         */
+
+        #endregion
     }
 }
