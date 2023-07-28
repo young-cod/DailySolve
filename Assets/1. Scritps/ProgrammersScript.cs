c2f326368f30efdf4fac6fcaf30897d7c60140ef
young-cod <64890861+young-cod@users.noreply.github.com>
2023-07-28 20:03:53 +0900

    외계어 사전

--- Assets/1. Scritps/ProgrammersScript.cs
+++ Assets/1. Scritps/ProgrammersScript.cs
@@ -1265,7 +1265,7 @@ image.png
 
         public static int Safezone(int[,] board)
         {
-            int answer = board.Length ;
+            int answer = board.Length;
             int[] x = { -1, 0, 1, -1, 1, -1, 0, 1 };
             int[] y = { -1, -1, -1, 0, 0, 1, 1, 1 };
             int len = board.GetLength(0);
@@ -1322,6 +1322,51 @@ sides의 길이는 2입니다.
             int answer = 0;
 
             answer = (sides.Max() + sides.Min()) - (sides.Max() - (sides.Min() - 1));
+
+            return answer;
+        }
+
+        #endregion
+
+        #region 외계어 사전
+
+        /*
+         * 문제 설명
+PROGRAMMERS-962 행성에 불시착한 우주비행사 머쓱이는 외계행성의 언어를 공부하려고 합니다. 알파벳이 담긴 배열 spell과 외계어 사전 dic이 매개변수로 주어집니다. spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1, 존재하지 않는다면 2를 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+spell과 dic의 원소는 알파벳 소문자로만 이루어져있습니다.
+2 ≤ spell의 크기 ≤ 10
+spell의 원소의 길이는 1입니다.
+1 ≤ dic의 크기 ≤ 10
+1 ≤ dic의 원소의 길이 ≤ 10
+spell의 원소를 모두 사용해 단어를 만들어야 합니다.
+spell의 원소를 모두 사용해 만들 수 있는 단어는 dic에 두 개 이상 존재하지 않습니다.
+dic과 spell 모두 중복된 원소를 갖지 않습니다.
+         */
+
+        public static int AlienDictionary(string[] spell, string[] dic)
+        {
+            int answer = 1;
+
+            foreach (var item in dic)
+            {
+                bool chk = true;
+                foreach (var ch in spell)
+                {
+                    if (!item.Contains(ch))
+                    {
+                        chk = false;
+                        break;
+                    }
+                }
+
+                if (chk)
+                {
+                    answer = 1; break;
+                }
+                else answer = 2;
+            }
 
             return answer;
         }
