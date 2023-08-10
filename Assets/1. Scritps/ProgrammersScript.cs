ae402720432ae4ee94cc234992c5b84741e09ae6
young-cod <64890861+young-cod@users.noreply.github.com>
2023-08-10 13:50:19 +0900

    로그인 성공?

--- Assets/1. Scritps/ProgrammersScript.cs
+++ Assets/1. Scritps/ProgrammersScript.cs
@@ -1636,22 +1636,16 @@ babbling의 각 문자열에서 "aya", "ye", "woo", "ma"는 각각 최대 한 번씩만 등장합니
         {
             int answer = 0;
             string[] zip = { "aya", "ye", "woo", "ma" };
-            string[] temp= { };
+
             //foreach 에선 elem 값의 변경이 불가(읽기전용)
             for (int i = 0; i < babbling.Length; i++)
             {
                 foreach (var str in zip)
                 {
-                    if (babbling[i].Contains(str) && babbling[i].Length > 1)
-                    {
-                        temp = babbling[i].Split(str);
-                    }
-                    foreach (var t in temp)
-                    {
-                        Debug.Log($"{t}----------");
-                    }
+                    babbling[i] = babbling[i].Replace(str, "-");
                 }
-                if (babbling[i].Length == 0) answer++;
+
+                if (babbling[i].Replace("-", "") == "") answer++;
             }
 
 
@@ -1659,5 +1653,52 @@ babbling의 각 문자열에서 "aya", "ye", "woo", "ma"는 각각 최대 한 번씩만 등장합니
         }
 
         #endregion
+
+        #region 로그인 성공?
+
+        /*
+         * 문제 설명
+머쓱이는 프로그래머스에 로그인하려고 합니다. 머쓱이가 입력한 아이디와 패스워드가 담긴 배열 id_pw와 회원들의 정보가 담긴 2차원 배열 db가 주어질 때, 다음과 같이 로그인 성공, 실패에 따른 메시지를 return하도록 solution 함수를 완성해주세요.
+
+아이디와 비밀번호가 모두 일치하는 회원정보가 있으면 "login"을 return합니다.
+로그인이 실패했을 때 아이디가 일치하는 회원이 없다면 “fail”를, 아이디는 일치하지만 비밀번호가 일치하는 회원이 없다면 “wrong pw”를 return 합니다.
+제한사항
+회원들의 아이디는 문자열입니다.
+회원들의 아이디는 알파벳 소문자와 숫자로만 이루어져 있습니다.
+회원들의 패스워드는 숫자로 구성된 문자열입니다.
+회원들의 비밀번호는 같을 수 있지만 아이디는 같을 수 없습니다.
+id_pw의 길이는 2입니다.
+id_pw와 db의 원소는 [아이디, 패스워드] 형태입니다.
+1 ≤ 아이디의 길이 ≤ 15
+1 ≤ 비밀번호의 길이 ≤ 6
+1 ≤ db의 길이 ≤ 10
+db의 원소의 길이는 2입니다.
+         */
+
+        public static string IsLoginSuccess(string[] id_pw, string[,] db)
+        {
+            string answer = "";
+
+            bool idChk = false;
+            bool pwChk = false;
+            for (int i = 0; i < db.GetLength(0); i++)
+            {
+                pwChk = false;
+
+                if (id_pw[0].Equals(db[i, 0])) idChk = true;
+                if (id_pw[1].Equals(db[i, 1]) && idChk) pwChk = true;
+
+                if (idChk & !pwChk) break;
+            }
+
+            if (idChk & pwChk) answer = "login";
+            else if (idChk && !pwChk) answer = "wrong pw";
+            else answer = "fail";
+
+            return answer;
+
+        }
+        #endregion
+
     }
 }
