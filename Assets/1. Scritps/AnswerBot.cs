d7d2875e4f4c832c6243e66fc148435819873673
young-cod <64890861+young-cod@users.noreply.github.com>
2023-08-10 13:34:48 +0900

    옹알이(1)

--- Assets/1. Scritps/AnswerBot.cs
+++ Assets/1. Scritps/AnswerBot.cs
@@ -7,14 +7,9 @@ public class AnswerBot : MonoBehaviour
 {
     void Start()
     {
-        int[] array = { 10, 2 };
-        int[,] array2 = { { 80, 70 }, { 90, 50 }, { 40, 70 }, { 50, 80 } };
-        int[,] array3 = { {80, 70}, {70, 80}, {30, 50}, {90, 100}, {100, 90}, {100, 100}, {10, 30} };
-        
-        foreach (var item in Solution.DetermineGrade(array3))
-        {
-            Debug.Log(item);
-        }
+        string[] str = { "aya", "yee", "u", "maa", "wyeoo" };
+
+        Debug.Log(Solution.Babbling(str));
 
         //foreach (var item in Solution.UniqueArray(array2, 30))
         //{
