d027491c30f3a082a6121b16d640e3a5515299ff
young-cod <64890861+young-cod@users.noreply.github.com>
2023-08-14 17:11:57 +0900

    치킨 쿠폰

--- Assets/1. Scritps/ProgrammersScript.cs
+++ Assets/1. Scritps/ProgrammersScript.cs
@@ -1700,5 +1700,37 @@ db의 원소의 길이는 2입니다.
         }
         #endregion
 
+        #region 치킨 쿠폰
+
+        /*
+         * 문제 설명
+프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다. 쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고, 서비스 치킨에도 쿠폰이 발급됩니다. 시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+chicken은 정수입니다.
+0 ≤ chicken ≤ 1,000,000
+         */
+
+        public static int ChickenCoupon(int chicken)
+        {
+            int answer = 0;
+            int coupon = chicken;
+
+            int order = 0;
+            while (coupon > 9)
+            {
+                order = 0;
+                order = coupon / 10;
+                coupon -= order * 10;
+                coupon += order;
+
+                answer += order;
+            }
+
+            return answer;
+        }
+
+        #endregion
+
     }
 }
