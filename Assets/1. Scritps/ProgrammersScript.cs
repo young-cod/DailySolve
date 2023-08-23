3ddbe37e5ed6a0e739d4499564640bd73ee56da4
young-cod <64890861+young-cod@users.noreply.github.com>
2023-08-23 17:16:55 +0900

    다음에 올 숫자

--- Assets/1. Scritps/ProgrammersScript.cs
+++ Assets/1. Scritps/ProgrammersScript.cs
@@ -1886,5 +1886,40 @@ num개의 연속된 수를 더하여 total이 될 수 없는 테스트 케이스는 없습니다.
         #endregion
 
         #endregion
+
+        #region 다음에 올 숫자
+
+        /*
+         * 문제 설명
+등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
+
+제한사항
+2 < common의 길이 < 1,000
+-1,000 < common의 원소 < 2,000
+common의 원소는 모두 정수입니다.
+등차수열 혹은 등비수열이 아닌 경우는 없습니다.
+등비수열인 경우 공비는 0이 아닌 정수입니다.
+         */
+
+        public static int NextNumberToCome(int[] common)
+        {
+            int answer = 0;
+
+            int first = common[1] - common[0];
+            int second = common[2] - common[1];
+
+            bool chk = false;
+            chk = first == second ? true : false;
+
+            if (chk == false)
+            {
+                int multi = common[1] / common[0];
+                answer = common[common.Length - 1] * multi;
+            }
+            else answer = common[common.Length - 1] + first;
+
+            return answer;
+        }
+        #endregion
     }
 }
